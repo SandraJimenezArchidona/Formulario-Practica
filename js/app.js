f3b56894a0b819e4deae8fc5f0fcc45cd21f52f6
Sansandra <106440634+SandraJimenezArchidona@users.noreply.github.com>
2022-06-29 13:34:24 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$('.header__menu').click(function(){
+
+    $('.header__der').addClass('activo')
+    
+})
+
+$('.item.arriba').mouseout(function(){
+
+    $('.electronica').removeClass('activo')
+    
+})
